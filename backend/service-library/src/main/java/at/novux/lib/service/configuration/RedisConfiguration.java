9539029c001a00668c3bec778176231f57199b0c
matthias-kopeinigg <matthiaskopeinigg@gmail.com>
2024-12-14 12:00:08 +0100

    Add default redis configuration

---
+++ backend/service-library/src/main/java/at/novux/lib/service/configuration/RedisConfiguration.java
@@ -0,0 +1,20 @@
+package at.novux.lib.service.configuration;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@Configuration
+@ConditionalOnProperty(name = "spring.data.redis.host")
+public class RedisConfiguration {
+
+    @Bean
+    public RedisTemplate<String, byte[]> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, byte[]> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        return template;
+    }
+
+}
